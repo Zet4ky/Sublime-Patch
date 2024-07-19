1de8ebcc1b2eebe8a154d2917f2e056b718ed17e
Zet4ky <146990006+Zet4ky@users.noreply.github.com>
2024-07-19 21:56:24 +0200

    Update subl_patch.ps1

--- subl_patch.ps1
+++ subl_patch.ps1
@@ -2,6 +2,8 @@
  * Credits: https://gist.github.com/skoqaq/3f3e8f28e23c881143cef9cf49d821ff?permalink_comment_id=4669272#gistcomment-4669272
 #>
 
+$ErrorActionPreference = "Continue"
+
 $basePath = "C:\Program Files\Sublime Text"
 $filePath = Join-Path $basePath "sublime_text.exe"
 
