af67985516acc6c82608757f0b13a24a3e6077e5
Zet4ky <146990006+Zet4ky@users.noreply.github.com>
2024-07-19 21:45:32 +0200

    Update subl_patch.ps1

--- subl_patch.ps1
+++ subl_patch.ps1
@@ -1,19 +1,4 @@
 <#
- * Copyright (C) 2024 Zetaky
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- *
  * Credits: https://gist.github.com/skoqaq/3f3e8f28e23c881143cef9cf49d821ff?permalink_comment_id=4669272#gistcomment-4669272
 #>
 
